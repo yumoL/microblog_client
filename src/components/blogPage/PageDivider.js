c0f55ea06ca55109debab22fe542cfd404107c48
luoyumo <yumo.luo@hotmail.com>
2020-02-28 15:59:25 +0200

    add PropTypes

--- src/components/blogPage/PageDivider.js
+++ src/components/blogPage/PageDivider.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Button, Divider } from 'semantic-ui-react'
+import PropTypes from 'prop-types'
 
 const PageDivider = props => {
   const { userId, getBlogsByUser, noMore } = props
@@ -10,7 +11,7 @@ const PageDivider = props => {
     setPageIndex(pageIndex+1)
     getBlogsByUser(userId, pageIndex)
   }
-  if(noMore){
+  if(noMore === true){
     return (
       <div>
         <Divider horizontal>end</Divider>
@@ -19,12 +20,18 @@ const PageDivider = props => {
   }
   return (
     <div>
-      <Button color='blue' onClick={handleLoadMore}>
+      <Button id='loadMoreButton' color='blue' onClick={handleLoadMore}>
       Click to load more
       </Button>
     </div>
 
   )
+}
+
+PageDivider.propTypes = {
+  getBlogsByUser: PropTypes.func.isRequired,
+  userId: PropTypes.string.isRequired,
+  noMore: PropTypes.bool.isRequired
 }
 
 export default PageDivider
